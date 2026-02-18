1a15f711b889228bcf91028e2c282d512d3269d3
Francisco <franciscorosa.work@gmail.com>
2026-02-18 16:30:21 +0000

    First commit

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -7,7 +7,6 @@ namespace MeuPrograma
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
-            Console.WriteLine("Bye World!");
         }
     }
 }
