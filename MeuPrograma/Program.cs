2d383e812c746c682600b8ed0b48d8a139aed841
Francisco <franciscorosa.work@gmail.com>
2026-02-18 17:03:39 +0000

    Add Commentary

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Cool class!
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// This prints phrases!
+        /// </summary>
+        /// <param name="args">Cool agrs :D!</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
