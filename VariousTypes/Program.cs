3831bb34f5b791921373a887044581a05e33e652
Francisco <franciscorosa.work@gmail.com>
2026-02-19 15:07:25 +0000

    First commit

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int sMaxHP = -10;
+            short sidade = -18;
+            sbyte slvl = -5;
+            long sBossHP = -999999999;
+            char copyrightSymbol = '\u2764';
+            byte lvl = 5;
+            ushort idade = 18;
+            uint MaxHP = 10;
+            ulong BossHP = 999999999;
+
+
+
+
+            Console.WriteLine(MaxHP);
+            Console.WriteLine(copyrightSymbol);
+            Console.WriteLine(sMaxHP);
+            Console.WriteLine(idade);
+            Console.WriteLine(sidade);
+            Console.WriteLine(lvl);
+            Console.WriteLine(slvl);
+            Console.WriteLine(BossHP);
+            Console.WriteLine(sBossHP)
+        }
+    }
+}
