75b2f632ce3ac470d88e51e41987d9a9550e1d96
Francisco <franciscorosa.work@gmail.com>
2026-02-19 16:58:18 +0000

    New Project Name

---
+++ Extra01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extra01
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("00AE");
+        }
+    }
+}
