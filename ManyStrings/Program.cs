87d7c73d8c06c57f6b30a9d4b342381fdd229598
Francisco <franciscorosa.work@gmail.com>
2026-06-02 17:14:38 +0100

    Add folder hellomsg and string unicode character with output

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string hellomsg = "Hello!";
+            string character = "\u20AC";
+
+            Console.WriteLine(hellomsg);
+            Console.WriteLine(character);
+        }
+    }
+}
