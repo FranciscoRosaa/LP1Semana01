3c9ac783d62a2c57d515563fb4a5960733615fba
Francisco <franciscorosa.work@gmail.com>
2026-06-02 17:23:19 +0100

    Add message and directory path output to Program.cs

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -11,9 +11,14 @@ namespace ManyStrings
 
             string hellomsg = "Hello!";
             string character = "\u20AC";
+            string message = "Good Day!";
+            message = "Started Raining :( | Bad Day!";
+            string dir_path = "C:\\Users\\João\\Downloads";
 
             Console.WriteLine(hellomsg);
             Console.WriteLine(character);
+            Console.WriteLine(message);
+            Console.WriteLine(dir_path);
         }
     }
 }
